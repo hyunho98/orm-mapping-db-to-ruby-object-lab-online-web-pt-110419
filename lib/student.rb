74d1817c5f4e364e3ff58465ed4fefe46add6c63
Alex Lee <hyunho98@gmail.com>
2020-02-05 03:17:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
     sql = <<-SQL
-      SELECT * FROM students WHERE name = ?;
+      SELECT * FROM students WHERE name = ? LIMIT 1;
     SQL
 
     self.new_from_db(DB[:conn].execute(sql, name))
