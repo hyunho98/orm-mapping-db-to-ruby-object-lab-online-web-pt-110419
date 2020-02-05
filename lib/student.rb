eec0bbf3e3b740bc999a30c234e686f86b895566
Alex Lee <hyunho98@gmail.com>
2020-02-05 03:27:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,4 +83,16 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?;
+    SQL
+
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
 end
