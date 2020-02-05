31cc081004018d8cd37a1772818009f1ef4339d8
Alex Lee <hyunho98@gmail.com>
2020-02-05 03:15:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,9 @@ class Student
       SELECT * FROM students;
     SQL
 
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.find_by_name(name)
